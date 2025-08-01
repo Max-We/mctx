6f6745ab7f3767418e4ed47a91a9e80fb924254a
Maximilian Weichart <maximilian.weichart@icloud.com>
2025-08-01 23:27:09 +0200

    value policy

--- mctx/_src/action_selection.py
+++ mctx/_src/action_selection.py
@@ -90,6 +90,86 @@ def muzero_action_selection(
   return masked_argmax(to_argmax, tree.root_invalid_actions * (depth == 0))
 
 
+def uct_action_selection(
+    rng_key: chex.PRNGKey,
+    tree: tree_lib.Tree,
+    node_index: chex.Numeric,
+    depth: chex.Numeric,
+    *,
+    pb_c_init: float = 1.25,
+    pb_c_base: float = 19652.0,
+    qtransform: base.QTransform = qtransforms.qtransform_by_parent_and_siblings,
+) -> chex.Array:
+  """Returns the action selected for a node index.
+
+  See Appendix B in https://arxiv.org/pdf/1911.08265.pdf for more details.
+
+  Args:
+    rng_key: random number generator state.
+    tree: _unbatched_ MCTS tree state.
+    node_index: scalar index of the node from which to select an action.
+    depth: the scalar depth of the current node. The root has depth zero.
+    pb_c_init: constant c_1 in the PUCT formula.
+    pb_c_base: constant c_2 in the PUCT formula.
+    qtransform: a monotonic transformation to convert the Q-values to [0, 1].
+
+  Returns:
+    action: the action selected from the given node.
+  """
+  visit_counts = tree.children_visits[node_index]
+  node_visit = tree.node_visits[node_index]
+  value_score = qtransform(tree, node_index)
+  explore_score = 1.25 * jnp.sqrt(jnp.log(node_visit) / visit_counts)
+
+  # Add tiny bit of randomness for tie break
+  node_noise_score = 1e-7 * jax.random.uniform(
+      rng_key, (tree.num_actions,))
+  to_argmax = value_score + explore_score + node_noise_score
+
+  # Masking the invalid actions at the root.
+  return masked_argmax(to_argmax, tree.root_invalid_actions * (depth == 0))
+
+
+def uct_normal_action_selection(
+    rng_key: chex.PRNGKey,
+    tree: tree_lib.Tree,
+    node_index: chex.Numeric,
+    depth: chex.Numeric,
+    *,
+    pb_c_init: float = 1.25,
+    pb_c_base: float = 19652.0,
+    qtransform: base.QTransform = qtransforms.qtransform_by_parent_and_siblings,
+) -> chex.Array:
+  """Returns the action selected for a node index.
+
+  See Appendix B in https://arxiv.org/pdf/1911.08265.pdf for more details.
+
+  Args:
+    rng_key: random number generator state.
+    tree: _unbatched_ MCTS tree state.
+    node_index: scalar index of the node from which to select an action.
+    depth: the scalar depth of the current node. The root has depth zero.
+    pb_c_init: constant c_1 in the PUCT formula.
+    pb_c_base: constant c_2 in the PUCT formula.
+    qtransform: a monotonic transformation to convert the Q-values to [0, 1].
+
+  Returns:
+    action: the action selected from the given node.
+  """
+  visit_counts = tree.children_visits[node_index]
+  node_visit = tree.node_visits[node_index]
+  value_score = qtransform(tree, node_index)
+  explore_score = 1.25 * jnp.sqrt(jnp.log(node_visit) / visit_counts)
+
+  # Add tiny bit of randomness for tie break
+  node_noise_score = 1e-7 * jax.random.uniform(
+      rng_key, (tree.num_actions,))
+  to_argmax = value_score + explore_score + node_noise_score
+
+  # Masking the invalid actions at the root.
+  return masked_argmax(to_argmax, tree.root_invalid_actions * (depth == 0))
+
+
 @chex.dataclass(frozen=True)
 class GumbelMuZeroExtraData:
   """Extra data for Gumbel MuZero search."""
