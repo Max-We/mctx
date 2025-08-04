5a1a59ba0fd482d16a4eb1b4075c305c21fcc4d9
Maximilian Weichart <maximilian.weichart@icloud.com>
2025-08-04 19:00:44 +0200

    uct bayes

--- mctx/__init__.py
+++ mctx/__init__.py
@@ -31,6 +31,7 @@ from mctx._src.base import RootFnOutput
 from mctx._src.policies import gumbel_muzero_policy
 from mctx._src.policies import muzero_policy
 from mctx._src.policies import muzero_uct_tuned_policy
+from mctx._src.policies import muzero_uct_bayes_policy
 from mctx._src.policies import stochastic_muzero_policy
 from mctx._src.qtransforms import qtransform_by_min_max
 from mctx._src.qtransforms import qtransform_by_parent_and_siblings
@@ -60,6 +61,7 @@ __all__ = (
     "muzero_action_selection",
     "muzero_policy",
     "muzero_uct_tuned_policy",
+    "muzero_uct_bayes_policy",
     "qtransform_by_min_max",
     "qtransform_by_parent_and_siblings",
     "qtransform_completed_by_mix_value",
